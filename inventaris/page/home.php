b2412169dc69cacec556266f7be36f482b01e62b
Riyanzakaria <150990276+Riyanzakaria@users.noreply.github.com>
2024-02-07 07:12:20 +0700

    Delete inventaris directory

--- inventaris/page/home.php
+++
@@ -1,6 +0,0 @@
-<div class="jumbotron">
-  <h1>Hello, <?= $user ?>!</h1>
-  <p>Selamat datang di aplikasi INVENTARIS</p>
-  <p>Aplikasi ini merupakan aplikasi uji level XII RPL</p>
-  <p><a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a></p>
-</div>
